3d99f00986c8cba10b4b10869a541b019d75e90b
zakharovKonstantin <99394095+zakharovKonstantin@users.noreply.github.com>
2022-06-07 19:44:44 +0500

    added content

---
+++ js/main.js
@@ -0,0 +1,5 @@
+const akita_items = document.getElementsByClassName('w__akita-items');
+const width_akita_items = akita_items.offsetWidth;
+const availableScreenWidth = window.screen.availWidth;
+console.log(availableScreenWidth);
+console.log(width_akita_items);
